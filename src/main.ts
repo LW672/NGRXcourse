d50fd3136035ac5789274eadd9e17e2dc42ecfbc
LW672 <v123121@gmail.com>
2025-02-19 22:42:04 +0200

    Working app ngrx_start

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+import {bootstrapApplication} from "@angular/platform-browser";
+import {AppComponent} from "./app/app.component";
+import {provideRouter} from "@angular/router";
+import {appRoutes} from "./app/app-routes";
+import {provideStore} from "@ngrx/store";
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, { providers: [provideRouter(appRoutes), provideStore()] });
